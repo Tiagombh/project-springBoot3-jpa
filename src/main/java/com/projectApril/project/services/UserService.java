d56b11fc7c1cdc13e999acf791608cd8e3554a9e
Tiago Magalhães <tiagomagalhaes.bh@gmail.com>
2025-04-04 15:35:29 +0100

    Service layer, component registration

---
+++ src/main/java/com/projectApril/project/services/UserService.java
@@ -0,0 +1,27 @@
+package com.projectApril.project.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projectApril.project.entities.User;
+import com.projectApril.project.repositories.UserRepository;
+
+import jakarta.persistence.Id;
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
