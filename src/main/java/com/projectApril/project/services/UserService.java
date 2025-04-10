ed8b2078f5b9906e14a4989d6cadcd16df281ac8
Tiago Magalhães <tiagomagalhaes.bh@gmail.com>
2025-04-10 22:54:20 +0100

    Exception handling part 1

--- src/main/java/com/projectApril/project/services/UserService.java
+++ src/main/java/com/projectApril/project/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.projectApril.project.entities.User;
 import com.projectApril.project.repositories.UserRepository;
+import com.projectApril.project.services.exceptions.ResourceNotFoundException;
 @Service
 public class UserService {
 	@Autowired
@@ -18,7 +19,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	 public User insert(User obj) {
 		 return repository.save(obj);
