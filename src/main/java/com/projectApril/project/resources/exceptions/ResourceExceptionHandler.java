14536effd27a130f77ecd0d895a634642ce5490a
Tiago Magalhães <tiagomagalhaes.bh@gmail.com>
2025-04-11 11:43:59 +0100

    Exception handling part 2

--- src/main/java/com/projectApril/project/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projectApril/project/resources/exceptions/ResourceExceptionHandler.java
@@ -2,11 +2,13 @@ package com.projectApril.project.resources.exceptions;
 
 import java.time.Instant;
 
+import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.projectApril.project.services.exceptions.DataBaseException;
 import com.projectApril.project.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,4 +24,27 @@ public class ResourceExceptionHandler {
 		StandardError err= new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 				return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request) {
+		String error = "DataBase Error";
+		HttpStatus status=HttpStatus.BAD_REQUEST;
+		StandardError err= new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+				return ResponseEntity.status(status).body(err);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/projectApril/project/services/UserService.java
+++ src/main/java/com/projectApril/project/services/UserService.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projectApril.project.entities.User;
 import com.projectApril.project.repositories.UserRepository;
+import com.projectApril.project.services.exceptions.DataBaseException;
 import com.projectApril.project.services.exceptions.ResourceNotFoundException;
+
 @Service
 public class UserService {
 	@Autowired
@@ -17,39 +21,38 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	 public User insert(User obj) {
-		 return repository.save(obj);
-	 }
-	 public void delete(Long id) {
-		 repository.deleteById(id);
-	 }
-	 public User update(Long id, User obj) {
-		 User entity= repository.getReferenceById(id);
+
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-	 }
+	}
+
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-	
+
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
